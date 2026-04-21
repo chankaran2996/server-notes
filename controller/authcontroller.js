3e4e40aa69a6b968fd0d46cb5ab28156292540ca
chankaran2996 <chandrasekaran2996@gmail.com>
2026-04-21 12:34:28 +0530

    register user in database

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,11 +1,28 @@
+import User from "../models/userModel.js";
 
 export const allUser = (req, res) => {
     res.status(200).json({message: "List of all users"});
 }
 
 
-export const register = (req, res) => {
-    res.status(201).json({message: "User registered successfully"});
+export const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        const responce = await User.create({
+            name,
+            email,
+            password
+        })
+
+        res.status(201).json({message: "User registered successfully", user: responce});
+        
+    } catch (error) {
+        res.status(500).json({message: "Internal server error", error: error.message});
+    }
 }
 
 export const login = (req, res) => {    
