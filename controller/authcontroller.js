0fbf4b97cfdffba6b4c7c06a4513324afbc1aa80
chankaran2996 <chandrasekaran2996@gmail.com>
2026-04-22 10:00:46 +0530

    generate token and login functions

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 export const allUser = (req, res) => {
     res.status(200).json({message: "List of all users"});
@@ -10,6 +11,10 @@ export const register = async (req, res) => {
         const { name, email, password } = req.body;
         if (!name || !email || !password) {
             return res.status(400).json({message: "All fields are required"});
+        }
+        const findUser = await User.findOne({email});
+        if (findUser) {
+            return res.status(400).json({message: "User already exists"});
         }
 
         const responce = await User.create({
@@ -25,8 +30,27 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {    
-    res.status(200).json({message: "User logged in successfully"});
+export const login = async (req, res) => {    
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({message: "Email and password are required"});
+        }
+
+        const findUser = await User.findOne({email});
+        if (!findUser) {
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+        const isMatch = await findUser.comparePassword(password);
+        if (!isMatch) {
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+        const token = generateToken(findUser._id);
+        res.status(200).json({message: "User logged in successfully", token});
+
+    } catch (error) {
+        res.status(500).json({message: "Internal server error", error: error.message});
+    }
 }
 
 export const updateProfile = (req, res) => {
